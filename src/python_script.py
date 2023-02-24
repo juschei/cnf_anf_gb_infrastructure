b9b383f64122d4ce3cbcfe69f889b9f23769cf9c
juschei <pocadev@hotmail.com>
2023-02-24 08:50:39 +0100

    test

---
+++ src/python_script.py
@@ -0,0 +1,8 @@
+import random
+
+
+def test():
+    random_int = random.randint(1,100)
+    filename = "/home/juschei/Desktop/IO/testfile_" + str(random_int)
+    with open(filename, "w") as f:
+        f.write("content written to file!")
